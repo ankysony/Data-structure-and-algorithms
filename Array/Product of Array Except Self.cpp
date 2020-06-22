1db52d76b482d5be3d23892acde87391d34ce186
ankit soni <ankisoni453@gmail.com>
2020-06-22 22:30:00 +0530

    updating

---
+++ Array/Product of Array Except Self.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+public:
+    vector<int> productExceptSelf(vector<int> &a)
+    {
+        vector<int> left, right, ans;
+        int n = a.size();
+        int d = 1;
+        for (int i = 0; i < n; i++)
+        {
+            d = d * a[i];
+            left.push_back(d);
+        }
+        d = 1;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            d = d * a[i];
+            right.push_back(d);
+        }
+
+        reverse(right.begin(), right.end());
+        for (int i = 0; i < n; i++)
+        {
+            if (i == 0)
+            {
+                ans.push_back(right[i + 1]);
+            }
+            else if (i == n - 1)
+            {
+                ans.push_back(left[i - 1]);
+            }
+            else
+            {
+                ans.push_back(right[i + 1] * left[i - 1]);
+            }
+        }
+        return ans;
+    }
+};
